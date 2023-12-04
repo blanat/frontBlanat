64ddb483efb3aa492f6f4dc6d0a97ee68f6b69d2
ChaimaaLou <louadnichaima@gmail.com>
2023-12-04 21:31:48 +0100

    Fixing signin

--- app/src/main/java/com/example/myapplication/UI/SignupScreen.java
+++ app/src/main/java/com/example/myapplication/UI/SignupScreen.java
@@ -2,9 +2,12 @@ package com.example.myapplication.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
@@ -25,8 +28,16 @@ public class SignupScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
-
+        final TextView linkLogIn = findViewById(R.id.linkLogIn);
         initializeComponents();
+
+        linkLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent = new Intent(SignupScreen.this, LoginScreen.class);
+                startActivity(signupIntent);
+            }
+        });
     }
 
     private void initializeComponents() {
