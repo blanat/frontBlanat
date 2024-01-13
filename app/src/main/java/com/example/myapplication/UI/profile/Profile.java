c1933b59aaf7444491ac2685981be292f097a6f4
ChaimaaLou <louadnichaima@gmail.com>
2024-01-13 19:52:06 +0100

    fixe

--- app/src/main/java/com/example/myapplication/UI/profile/Profile.java
+++ app/src/main/java/com/example/myapplication/UI/profile/Profile.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
