450c28dab97ef055e4147075b19919b125ab0142
bensar oumaima <110025535+OumaimaBensar@users.noreply.github.com>
2024-01-16 12:33:56 +0100

    retrofit changes

--- app/src/main/java/com/example/myapplication/retrofit/RetrofitService.java
+++ app/src/main/java/com/example/myapplication/retrofit/RetrofitService.java
@@ -42,7 +42,7 @@ public class RetrofitService {
 
         // Build the Retrofit instance with the OkHttpClient and GsonConverterFactory
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://105.129.76.25:8085")
+                .baseUrl("http://10.158.84.2:8085")
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(okHttpClient)
                 .build();
